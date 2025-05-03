3041ddff4e198761d82b5dacd8c2f5580985efad
Idontknowhowtotype <eerror754@gmail.com>
2025-05-03 21:36:27 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-print("Updating script! Also if you want me to make become gamepass role for free, dm me (Im poor so you will have to give me account / buy me the role")
+print("Updating script! Also if you want me to make become gamepass role for free, dm me - .banned.user (Im poor so you will have to give me account / buy me the role")
